2b7567726177a395d83e3049a68414e72fa9975b
Darren Corbett <darrentcorbett@gmail.com>
2018-02-14 15:06:32 +0000

    try hard coded test

--- index.js
+++ index.js
@@ -1,39 +1,37 @@
 /* jshint node: true */
-'use strict';
 
-var path = require('path');
-var FeatureParser = require('./lib/feature-parser');
+const path = require('path');
+const FeatureParser = require('./lib/feature-parser');
 
 module.exports = {
   name: 'ember-cli-yadda',
-  getTestFramework: function() {
-    var packages = Object.keys(this.project.addonPackages);
+  getTestFramework() {
+    const packages = Object.keys(this.project.addonPackages);
     if (packages.indexOf('ember-cli-mocha') > -1) {
       return 'mocha';
-    } else {
-      return 'qunit';
     }
+    return 'qunit';
   },
-  setupPreprocessorRegistry: function(type, registry) {
-    var testFramework = this.getTestFramework();
-    var self = this;
+  setupPreprocessorRegistry(type, registry) {
+    const testFramework = this.getTestFramework();
+    const self = this;
     registry.add('js', {
       name: 'ember-cli-yadda',
       ext: ['feature', 'spec', 'specification'],
-      toTree: function(tree, inputPath, outputPath) {
+      toTree(tree) {
         return new FeatureParser(tree, testFramework, self.options);
-      }
+      },
     });
   },
-  blueprintsPath: function() {
+  blueprintsPath() {
     return path.join(__dirname, 'blueprints', this.getTestFramework());
   },
-  included: function(app) {
+  included(app) {
     this._super.included(app);
-    var options = app.options['ember-cli-yadda'] || {};
+    const options = app.options['ember-cli-yadda'] || {};
     if (typeof options.persist === 'undefined') {
       options.persist = true;
     }
     this.options = options;
-  }
+  },
 };
